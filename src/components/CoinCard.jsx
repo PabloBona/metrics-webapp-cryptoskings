9a4befcb56f0b5c1807735419888926d5fa2619a
PabloBona <pablobonasera@gmail.com>
2023-08-01 22:34:02 -0300

    Add styles

--- src/components/CoinCard.jsx
+++ src/components/CoinCard.jsx
@@ -11,11 +11,14 @@ const CoinCard = ({ coin }) => (
             <img className="iconStyle p-1 rounded-5" src={coin.icon} alt="" />
           </div>
           <div>
-            <h3 className="coin-name fw-bold my-3 text-center text-white">{coin.name}</h3>
+            <h3 className="coin-name my-3 text-center text-white">{coin.name}</h3>
           </div>
           <div className="align-self-center">
             <p className="fs-5 text-center text-white m-0">
               {`$${coin.price.toFixed(5)}`}
+            </p>
+            <p className="fs-6 text-center text-white m-0">
+              {`Rank ${coin.rank}`}
             </p>
           </div>
         </div>
@@ -30,6 +33,7 @@ CoinCard.propTypes = {
     icon: PropTypes.string.isRequired,
     name: PropTypes.string.isRequired,
     price: PropTypes.number.isRequired,
+    rank: PropTypes.number.isRequired,
   }).isRequired,
 };
 
